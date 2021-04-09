330a164959aaeeae9cd6f3a0abefcdb632b811ad
Piaczio <trotroyu@gmail.com>
2021-04-08 19:57:22 -0600

    conflics

--- js/mecanic.js
+++ js/mecanic.js
@@ -1,27 +1,7 @@
 
-<<<<<<< HEAD
-let input;
-
-let output = [];
-/*let suma = (input) => {
-    input = document.getElementById('input').value;
-    output = input;
-    document.getElementById('output').innerHTML = output;
-    return output;
-}*/
-
-function splitToDigit(n) {
-     return [...n + ''].map(Number)
-}
-/*function splitToDigit(n){
-  return (n + '').split('').map((i) => { return Number(i); })
-} */
-let atall = (input) => {
-=======
 /*let input;
 let output;
 var suma = () => {
->>>>>>> master
      input = document.getElementById('input').value;
      input = splitToDigit(input);
      input = input.sort((a, b) => b - a);
@@ -30,7 +10,4 @@ var suma = () => {
      document.getElementById('output').innerHTML = output;
      return output;
 }
-<<<<<<< HEAD
-=======
 suma();*/
->>>>>>> master

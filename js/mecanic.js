22b0960becf77ba7da4c81583cb3f22276718707
Piaczio <trotroyu@gmail.com>
2021-04-11 11:44:42 -0600

    fix: Using time as velocity relation.

--- js/mecanic.js
+++ js/mecanic.js
@@ -3,9 +3,18 @@ function insertSpeed() {
 
      let input;
      input = document.getElementById('input').value;
-     input = input + "s";//unir un string con numero contenido en una variable.
-     output = input;//guarda la entrada de datos para mostrarla en una una etiqueta posteriormente
-     document.getElementById('output').innerHTML = output;
-     dataSpeed.style.animation = "rotate " + input + " linear reverse infinite"
+     if (input >= 0 && input <= 20) {
+          output = input;//guarda la entrada de datos para mostrarla en una una etiqueta posteriormente
+          input = (500 / input);//
+          input = input + "s";//unir un string con numero contenido en una variable.
+          console.log(output);
+          document.getElementById('output').innerHTML = output;
+          dataSpeed.style.animation = "rotate " + input + " linear reverse infinite"
+     } else {
+          alert("Entra incorrecta, debes ingresar una velocidad entre 1 y 20 km/hr. Intentalo de nuevo porfavor.");
+     }
+
+
+
 
 }

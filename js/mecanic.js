1911bfb8e6d2f2bf828c4709b5842d198930b1a4
Piaczio <trotroyu@gmail.com>
2021-12-18 13:30:56 -0700

    nuevo texto de emergencia

--- js/mecanic.js
+++ js/mecanic.js
@@ -1,6 +1,5 @@
 function insertSpeed() {
      const dataSpeed = document.querySelector(".path-run");
-
      let input;
      input = document.getElementById('input').value;
      if (input >= 0 && input <= 20) {
@@ -10,10 +9,6 @@ function insertSpeed() {
           document.getElementById('output').innerHTML = output;
           dataSpeed.style.animation = "rotate " + input + " linear reverse infinite"
      } else {
-          alert("Entra incorrecta, debes ingresar una velocidad entre 1 y 20 km/hr. Intentalo de nuevo porfavor.");
+          alert("Entrada incorrecta, debes ingresar una velocidad entre 1 y 20 km/hr. Intentalo de nuevo porfavor.");
      }
-
-
-
-
 }
